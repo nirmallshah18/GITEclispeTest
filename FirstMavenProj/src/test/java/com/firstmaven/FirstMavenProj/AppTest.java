9dc32790764f56a8fb249bcee2628e05a8c2d1a6
ridhi <ridhi@LAPTOP-MJMP4U20>
2020-08-23 02:46:58 +0530

    Initial commit By Nirmal

---
+++ FirstMavenProj/src/test/java/com/firstmaven/FirstMavenProj/AppTest.java
@@ -0,0 +1,20 @@
+package com.firstmaven.FirstMavenProj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
